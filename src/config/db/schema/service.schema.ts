3f8ad7173f051021888cd7e902f25a58bca9ed3a
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-19 14:18:16 -0600

    fix: adding unique constraint on name column

--- src/config/db/schema/service.schema.ts
+++ src/config/db/schema/service.schema.ts
@@ -5,6 +5,6 @@ import { UUID } from 'node:crypto';
 
 export const serviceTable = pgTable('services', {
 	id: uuid('id').$type<UUID>().primaryKey().defaultRandom(),
-	name: varchar('name', { length: 128 }).notNull(),
+	name: varchar('name', { length: 128 }).notNull().unique(),
 	isExternal: boolean('is_external').notNull().default(false),
 });
